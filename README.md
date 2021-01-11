4284ea6caeff0a4044b686052db88e1cf5f09816
hefuweill <2995545463@qq.com>
2021-01-11 17:52:35 +0800

    Add: 145

--- README.md
+++ README.md
@@ -11,6 +11,7 @@
 
 * [144 二叉树的前序遍历](src/144/Solution.kt) 中等 [题目](https://leetcode-cn.com/problems/binary-tree-preorder-traversal/)
 * [94 二叉树的中序遍历](src/94/Solution.kt) 中等 [题目](https://leetcode-cn.com/problems/binary-tree-inorder-traversal/)
+* [145 二叉树的后序遍历](src/145/Solution.kt) 中等 [题目](https://leetcode-cn.com/problems/binary-tree-postorder-traversal/)
 
 ### 动态规划
 
---
+++ src/145/Solution.kt
@@ -0,0 +1,59 @@
+package `145`
+
+import bean.TreeNode
+import java.util.*
+
+/**
+ * 后序遍历 左右中 迭代时注意回溯，挺难的
+ */
+class Solution {
+
+    fun postorderTraversal(root: TreeNode?): List<Int> {
+//        return recursion(root)
+        return iteration(root)
+    }
+
+    private fun iteration(root: TreeNode?): List<Int> {
+        val resultList = mutableListOf<Int>()
+        val stack = Stack<TreeNode>()
+        var currentNode: TreeNode? = root
+        var previewNode: TreeNode? = null
+        while (!stack.isEmpty() || currentNode != null) {
+            while (currentNode != null) {
+                stack.push(currentNode)
+                currentNode = currentNode.left
+            }
+            val node = stack.peek()
+            if (node.right != null && node.right != previewNode) {
+                currentNode = node.right
+            } else {
+                previewNode = node
+                resultList.add(stack.pop().`val`)
+            }
+        }
+        return resultList
+    }
+
+    private fun recursion(node: TreeNode?): List<Int> {
+        val resultList = mutableListOf<Int>()
+        realRecursion(node, resultList)
+        return resultList
+    }
+
+    private fun realRecursion(node: TreeNode?, resultList: MutableList<Int>) {
+        if (node == null) {
+            return
+        }
+        realRecursion(node.left, resultList)
+        realRecursion(node.right, resultList)
+        resultList.add(node.`val`)
+    }
+}
+
+fun main() {
+    println(Solution().postorderTraversal(TreeNode(1).apply {
+        right = TreeNode(2).apply {
+            left = TreeNode(3)
+        }
+    }))
+}
