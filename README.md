b2d6c5f918f47cf4c0eaf06e1b451bdbf37552e9
hefuweill <2995545463@qq.com>
2021-01-11 18:36:08 +0800

    Add: 100

--- README.md
+++ README.md
@@ -12,6 +12,7 @@
 * [144 二叉树的前序遍历](src/144/Solution.kt) 中等 [题目](https://leetcode-cn.com/problems/binary-tree-preorder-traversal/)
 * [94 二叉树的中序遍历](src/94/Solution.kt) 中等 [题目](https://leetcode-cn.com/problems/binary-tree-inorder-traversal/)
 * [145 二叉树的后序遍历](src/145/Solution.kt) 中等 [题目](https://leetcode-cn.com/problems/binary-tree-postorder-traversal/)
+* [100 相同的树](src/100/Solution.kt) 简单 [题目](https://leetcode-cn.com/problems/same-tree/)
 
 ### 动态规划
 
---
+++ src/100/Solution.kt
@@ -0,0 +1,27 @@
+package `100`
+
+import bean.TreeNode
+
+/**
+ * 遍历一次如果都相同那么就是相同树，使用递归 DFS
+ */
+class Solution {
+
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        return if (p != null && q != null) {
+            p.`val` == q.`val` && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+        } else p == null && q == null
+    }
+}
+
+fun main() {
+    println(Solution().isSameTree(TreeNode(1).apply {
+        right = TreeNode(2).apply {
+            left = TreeNode(3)
+        }
+    }, TreeNode(1).apply {
+        right = TreeNode(2).apply {
+            left = TreeNode(3)
+        }
+    }))
+}
