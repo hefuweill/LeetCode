0f368623c52aeb88f15b5ff2f5a314a7d5bc3cfe
hefuweill <2995545463@qq.com>
2021-01-18 19:10:07 +0800

    Refactor: 标记数组处理全排列

--- src/46/Solution.kt
+++ src/46/Solution.kt
@@ -12,14 +12,20 @@ import kotlin.collections.ArrayList
  * [3, 1, 2]
  * [3, 2, 1]
  * 需要记住放入结果时需要拷贝一份，不然后续又会移除
+ * 第一种方式时间效率不高，如果想高效可以使用标记数组（第二种方式空间效率不高）
+ * 如果想两者兼顾，那么只能动态交换了（暂时忽略）
  */
 class Solution {
 
     fun permute(nums: IntArray): List<List<Int>> {
-        return recursion(nums, LinkedList(), arrayListOf())
+//        return recursion1(nums, LinkedList(), arrayListOf())
+        return recursion2(nums, BooleanArray(nums.size), LinkedList(), arrayListOf())
     }
 
-    private fun recursion(nums: IntArray, list: LinkedList<Int>, resultList: MutableList<List<Int>>): List<List<Int>> {
+    /**
+     * 查找法
+     */
+    private fun recursion1(nums: IntArray, list: LinkedList<Int>, resultList: MutableList<List<Int>>): List<List<Int>> {
         if (list.size == nums.size) {
             resultList.add(ArrayList(list))
             return resultList
@@ -29,7 +35,28 @@ class Solution {
                 continue
             }
             list.add(nums[i])
-            recursion(nums, list, resultList)
+            recursion1(nums, list, resultList)
+            list.removeLast()
+        }
+        return resultList
+    }
+
+    /**
+     * 标记法
+     */
+    private fun recursion2(nums: IntArray, markNums: BooleanArray, list: LinkedList<Int>, resultList: MutableList<List<Int>>): List<List<Int>> {
+        if (list.size == nums.size) {
+            resultList.add(ArrayList(list))
+            return resultList
+        }
+        for (i in 0..nums.lastIndex) {
+            if (markNums[i]) {
+                continue
+            }
+            list.add(nums[i])
+            markNums[i] = true
+            recursion1(nums, list, resultList)
+            markNums[i] = false
             list.removeLast()
         }
         return resultList
